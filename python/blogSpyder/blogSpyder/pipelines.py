b8595273d7ec8fed2f2eb3eb3e58578dbb441a16
“generalgong” <generalgong@gmail.com>
2016-06-13 05:12:32 +0800

    添加分词库

--- python/blogSpyder/blogSpyder/pipelines.py
+++ python/blogSpyder/blogSpyder/pipelines.py
@@ -13,8 +13,9 @@ from items import BlogspyderItem
 class BlogspyderPipeline(object):
     def __init__(self):
         self.file =self.file = codecs.open('blogitems.json', mode='wb', encoding='utf-8')
+        jieba.load_userdict("/home/hduser/code/vmcode/python/pylibs/jiebiaDict/SougouITWords.txt")
     def process_item(self, item, spider):
-        item["pageContent"] =" / ".join(jieba.cut( html2text.html2text(''.join(item["pageContent"])), cut_all=True))
+        item["pageContent"] =" / ".join(jieba.cut( html2text.html2text(''.join(item["pageContent"])), cut_all=False))
         line = json.dumps(dict(item) )+'\n'
         self.file.write(line.decode("unicode_escape"))
         return item
