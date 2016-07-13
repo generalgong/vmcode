0df1082030e639b7344bdea484073f463ec9ea6c
“generalgong” <generalgong@gmail.com>
2016-07-14 04:07:59 +0800

    spark datafram

--- spark/gongPG/sample.sbt
+++ spark/gongPG/sample.sbt
@@ -2,6 +2,10 @@ name := "gongPR"
 
 version := "2.0"
 
-scalaVersion := "2.10.4"
+scalaVersion := "2.10.5"
 
-libraryDependencies += "org.apache.spark" %% "spark-core" % "1.4.1"
+libraryDependencies += "org.apache.spark" %% "spark-core" % "1.6.2"
+
+libraryDependencies += "mysql" % "mysql-connector-java" % "5.1.34"
+
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "1.6.2"
--- spark/gongPG/src/main/scala/gongPR.scala
+++ spark/gongPG/src/main/scala/gongPR.scala
@@ -13,22 +13,40 @@ object gongPR{
 
   def main(args: Array[String]) {
     if (args.length < 1) {
-      System.err.println("Usage: SparkPageRank <file> <iter>")
+      System.err.println("Usage: SparkPageRank <iter>")
       System.exit(1)
     }
     showWarning()
 
     val sparkConf = new SparkConf().setAppName("gongPageRank").setMaster("local[2]")
-    val iters = if (args.length > 1) args(1).toInt else 10
+    val iters = if (args.length > 1) args(0).toInt else 1
     val ctx = new SparkContext(sparkConf)
-    val lines = ctx.textFile(args(0), 1)
+    //val lines = ctx.textFile(args(0), 1)
+    val sqlContext = new org.apache.spark.sql.SQLContext(ctx)
+    val url="jdbc:mysql://brian1:3306/csdn"
+    val prop = new java.util.Properties
+    prop.setProperty("user","brian")
+    prop.setProperty("password","general")
+    val df = sqlContext.read.jdbc(url,"BlogIDs",prop)
+    
+    val pidFows=  df.select(df("pageID"), df("follows"))//.limit(10)
 
+    val lines  = pidFows.collectAsList
+    /*
     val links = lines.map{ s =>
       val parts = s.split("\\s+")
       (parts.head, parts.tail)
     }.sortBy(_._1).cache()
-    //Array((B,CompactBuffer(D)), (D,CompactBuffer(B)), (A,CompactBuffer(B)), (C,CompactBuffer(A)))
-    val pageSize = lines.count()
+    */
+   val links = pidFows.map{
+    //    s => val parts = s.toString.split(",")
+        s => val parts = s.mkString(",").split(",")
+        (parts.head, parts.tail)
+    }.sortBy(_._1).cache()
+
+   //Array((B,CompactBuffer(D)), (D,CompactBuffer(B)), (A,CompactBuffer(B)), (C,CompactBuffer(A)))
+   // val pageSize = lines.count()
+    val pageSize = lines.size() //if use dataframe, line is a List()
     var ranks = links.mapValues(v => 1.0 /pageSize).sortBy(_._1)
     //Array((B,1.0), (D,1.0), (A,1.0), (C,1.0))
 
@@ -44,7 +62,7 @@ object gongPR{
     val output = ranks.collect()
     //Array[(String, Double)] = Array((B,1.8499999999999999), (D,1.0), (A,1.0))
     output.foreach(tup => println(tup._1 + "gong: has rank: " + tup._2 + "."))
-
+    ranks.saveAsTextFile("/home/hduser/pageRank.spark")
     ctx.stop()
   }
 }
